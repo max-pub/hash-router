9c8aeb3ab670eeb71c7d85c7a4121ce938fa1295
Max Fechner <max.fechner@gmail.com>
2016-04-29 15:14:36 +0200

    hash-links
    highlight links with hash-url

---
+++ hash-links.js
@@ -0,0 +1,11 @@
+window.addEventListener('hashchange', function() {
+	var links = document.getElementsByTagName('a');
+	var hash = document.location.hash;
+	lll = links;
+	for (var i = 0; i < links.length; i++) {
+		var href = links[i].getAttribute('href');
+		// console.log(href, hash, links[i]);
+		if (href == hash) links[i].setAttribute('hash', 'active');
+		else links[i].setAttribute('hash', 'passive');
+	}
+});
