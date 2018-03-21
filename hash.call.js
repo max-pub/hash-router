4bb46fd7d81511175d92c378973e8e96c84b855d
Max Fechner <max.fechner@gmail.com>
2018-03-21 14:10:12 +0100

    ...

--- hash.call.js
+++ hash.call.js
@@ -1,12 +1,12 @@
 HASHCALL = (regex, callback) => {
 	var check = () => {
-		var hash = document.location.hash;
-		if (hash[0] === '#') hash = hash.substr(1);
-		if (hash[0] === '!') hash = hash.substr(1);
-		let match = new RegExp(regex).exec(hash);
-		// console.log('ma', match);
+		let match = new RegExp(regex).exec(document.location.hash.substr(1));
 		if (match) callback.apply(null, match.slice(1)) // call / apply
 	}
 	check();
 	window.addEventListener('hashchange', () => check());
 }
+
+// var hash = document.location.hash;
+// if (hash[0] === '#') hash = hash.substr(1);
+// if (hash[0] === '!') hash = hash.substr(1);
